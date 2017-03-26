b0e4bc4007bf92aa9ad2bed69f219f947da8d597
Prasad Katti <prasadmkatti@gmail.com>
2017-03-26 22:43:22 +0000

    Add more classifiers to setup.py
    [skip ci]

--- setup.py
+++ setup.py
@@ -4,7 +4,26 @@ import py_hangman
 LONG_DESCRIPTION = """
 This is a simple text-based 2-player version of Hangman.
 More description will be added later.
+
+This project supports Python 2.7 and Python 3.2 and higher.
 """
+
+PROJECT_CLASSIFIERS = [
+    'Development Status :: 3 - Alpha',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Intended Audience :: End Users/Desktop',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Games/Entertainment',
+    'Topic :: Games/Entertainment :: Puzzle Games',
+]
 
 setup(
     name="py_hangman",
@@ -15,19 +34,8 @@ setup(
     author="Prasad Katti",
     author_email="prasadmkatti@gmail.com",
     packages=find_packages(),
-    keywords='games hangman packaging',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Games/Entertainment',
-    ],
+    keywords='games hangman',
+    classifiers=PROJECT_CLASSIFIERS,
     entry_points={
         'console_scripts': [
             'hangman=py_hangman.hangman:main',
